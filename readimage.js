99be75cfdaf982d325989e979604a2d49fe55f92
Seojisoo20191941 <amy0480@naver.com>
2022-01-14 00:40:18 +0900

    feat:fix onclick event error

--- readimage.js
+++ readimage.js
@@ -10,9 +10,10 @@ function reading(){
     const arr=str.split(",");
 
     for(var i=0;i<arr.length;i++){
-        document.writeln(arr[i]);
+        // document.writeln(arr[i]);
         window.open(arr[i]);
+        console.log('클릭됨');
+
     }
-    console.log('클릭됨');
 }
 //onclick하면 함수부르는 방식으로.
