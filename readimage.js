be321d3c4d8b35ce63a98dfc94067567ad70570a
Seojisoo20191941 <amy0480@naver.com>
2022-01-14 00:32:20 +0900

    feat:fix js function

--- readimage.js
+++ readimage.js
@@ -4,11 +4,15 @@
 // for i in range(len(urls)):
 // 	webbrowser.open(urls[i])
 
-var str=confirm();
-const arr=str.split(",");
+function reading(){
+    // const str=confirm();
+    var str = document.getElementById('input').value;
+    const arr=str.split(",");
 
-for(var i=0;i<arr.length;i++){
-    document.writeln(arr[i]);
-    window.open(arr[i]);
+    for(var i=0;i<arr.length;i++){
+        document.writeln(arr[i]);
+        window.open(arr[i]);
+    }
+    console.log('클릭됨');
 }
 //onclick하면 함수부르는 방식으로.
