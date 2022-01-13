dccaf8c1e63ad816f77c50ccc8d12105f5c773df
Seojisoo20191941 <amy0480@naver.com>
2022-01-14 00:09:20 +0900

    add javascript code

--- readimage.js
+++ readimage.js
@@ -3,4 +3,11 @@
 // urls=urls.split(',')
 // for i in range(len(urls)):
 // 	webbrowser.open(urls[i])
-	
+
+var str=confirm();
+const arr=str.split(",");
+
+for(var i=0;i<arr.length;i++){
+    document.writeln(arr[i]);
+}
+alert(arr)
