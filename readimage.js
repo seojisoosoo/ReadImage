3959dc7ffe5c9fd75e0b9aea719eaf31a099e7b6
Seojisoo20191941 <amy0480@naver.com>
2022-09-28 00:05:05 +0900

    refactor

--- readimage.js
+++ readimage.js
@@ -4,16 +4,19 @@
 // for i in range(len(urls)):
 // 	webbrowser.open(urls[i])
 
-function reading(){
-    var str = document.getElementById('input').value;
-    const arr=str.split(",");
-    document.writeln(arr.length);
+function reading() {
+  var str = document.getElementById("input").value;
+  const arr = str.split(",");
+  // document.writeln(arr.length);
+  for (var i = 0; i < arr.length; i++) {
+    let a = `<a href=${arr[i]}>${arr[i]}</a>`;
+    document.writeln(a);
 
-    for(var i=0;i<arr.length;i++){
-        document.writeln(arr[i]);
-        window.open(arr[i],i);
-        console.log(i+'클릭됨');
+    // document.writeln(arr[i]);
 
-    }
+    // location.href = arr[i];
+    // window.open(arr[i], i);
+    console.log(i + "클릭됨");
+  }
 }
 //첫번째 페이지만 열림..
